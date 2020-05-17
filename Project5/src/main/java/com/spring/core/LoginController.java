3b3c1de70df52f047593bd9ebb7bd304f741623c
jdk-world <saurabh2204@gmail.com>
2020-05-17 17:13:18 +0530

    update

--- Project5/src/main/java/com/spring/core/LoginController.java
+++ Project5/src/main/java/com/spring/core/LoginController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-
+//http://localhost:8085/spring-mvc/hello
 @Controller
 public class LoginController {
 
